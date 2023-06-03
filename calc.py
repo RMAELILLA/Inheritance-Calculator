97b64f472a85241027b48a66b38bbd3fff68cae1
RMAELILLA <ronioryzamae12@gmail.com>
2023-06-03 22:01:13 +0800

    add elif for square root

--- calc.py
+++ calc.py
@@ -1,14 +1,16 @@
 from main_calc import CalculatorOperator
-from user_d import UserDisplay
+from main_user_d import UserDisplay
 from inheritance_calc import InheritanceCalc
+from inheritance_user_d import InheritanceUserD
 
 class Calc:
     def __init__(self):
         self.user_d = UserDisplay()
         self.inheritance_calc = CalculatorOperator()
         self.inheritance_calc = InheritanceCalc()
+        self.inheritance_ud = InheritanceUserD()
     def run(self):
-        math_operator = self.user_d.math_operator().lower()
+        math_operator = self.inheritance_ud.math_operator().lower()
 
         if math_operator in ["addition", "subtraction", "multiplication", "division", "square root"]:
             try:
@@ -27,12 +29,15 @@ class Calc:
                 elif math_operator == "division":
                     division = self.inheritance_calc.cal_divide(first_number, second_number)
                     self.user_d.display_division(division)
-                elif math_operator == "square root":
-                    sqr_rt = self.inheritance_calc.cal_sqr_rt(first_number)
-                    self.user_d.display_sqr_rt(sqr_rt)
             except ValueError:
                 print("Field cannot include non-integer or non-numerical values or be blank.")
-            
+        elif math_operator == "square root":
+            try:
+                first_number = self.user_d.user_number()
+                sqr_rt = self.inheritance_calc.cal_sqr_rt(first_number)
+                self.user_d.display_sqr_rt(sqr_rt)
+            except ValueError:
+                print("Field cannot include non-integer or non-numerical values or be blank.")
         else:
             print("I don't understant your input, please choose one only in the four math operators.")
 
